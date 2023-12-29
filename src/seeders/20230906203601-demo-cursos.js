9afbde56de02667c1ed0eb730f2c2efec57118ec
Eduardo Morais <dudu.vgvm@gmail.com>
2023-12-29 12:12:13 -0300

    Commit Inicial

--- src/seeders/20230906203601-demo-cursos.js
+++ src/seeders/20230906203601-demo-cursos.js
@@ -2,12 +2,21 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('cursos', [
       {
         titulo: 'API com Express',
         descricao: 'Curso de API com Express e MongoDB',
         data_inicio: '2023-01-01',
+        categoria_id: 1,
+        docente_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        titulo: 'API com GraphQL',
+        descricao: 'Curso de API com GraphQL e Apollo',
+        data_inicio: '2023-02-01',
         categoria_id: 1,
         docente_id: 5,
         createdAt: new Date(),
@@ -23,9 +32,27 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
+        titulo: 'Tópicos Avançados',
+        descricao: 'Curso de tópicos avançados com Java',
+        data_inicio: '2023-03-01',
+        categoria_id: 2,
+        docente_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
         titulo: 'Python Web com Django',
         descricao: 'Curso de aplicações web com Django',
         data_inicio: '2023-01-01',
+        categoria_id: 3,
+        docente_id: 6,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        titulo: 'Python Web com Django II',
+        descricao: 'Curso de aplicações web com Django - continuação',
+        data_inicio: '2023-05-01',
         categoria_id: 3,
         docente_id: 6,
         createdAt: new Date(),
@@ -40,10 +67,19 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+      {
+        titulo: 'Testes limpos com C#',
+        descricao: 'Curso de testes com C#',
+        data_inicio: '2023-04-01',
+        categoria_id: 4,
+        docente_id: 6,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ], {});
   },
 
-  async down(queryInterface, Sequelize) {
+  async down (queryInterface, Sequelize) {
     await queryInterface.bulkDelete('cursos', null, {});
   }
 };
