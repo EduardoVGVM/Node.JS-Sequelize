c43c833383cab470637ead0e6cabe46a082fde75
Eduardo Morais <dudu.vgvm@gmail.com>
2023-12-30 12:31:29 -0300

    Aula 3-2

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -8,13 +8,34 @@ class PessoaController extends Controller {
     super(pessoaServices);
   }
 
-  async pegaMatriculas(req, res) {
+  async pegaMatriculasAtivas(req, res) {
     const { estudanteId } = req.params;
     try {
-      const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+      const listaMatriculas = await pessoaServices.pegaMatriculasAtivasPorEstudante(Number(estudanteId));
       return res.status(200).json(listaMatriculas);
     } catch (erro) {
-      // erro
+      return res.status(500).json({erro: erro.message});
+    }
+  }
+
+  async pegaTodasAsMatriculas(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      const listaMatriculas = await pessoaServices.pegaTodasAsMatriculasPorEstudante(Number(estudanteId));
+      return res.status(200).json(listaMatriculas);
+    } catch (erro) {
+      return res.status(500).json({erro: erro.message});
+    }
+  }
+
+  async pegaTodasAsPessoas(req, res) {
+    try {
+      const listaTodasAsPessoas = await pessoaServices.pegaPessoasEscopoTodos();
+      return res.status(200).json(listaTodasAsPessoas);
+    }
+    
+    catch (erro) {
+      return res.status(500).json({erro: erro.message});
     }
   }
 }
